c1dede1431c4793ea7e8cc625dddc67784f1f483
Ehor Meschenko <meshhenko04@bk.ru>
2024-06-24 22:47:08 +0300

    add full task 2

--- public_project/main/views.py
+++ public_project/main/views.py
@@ -13,19 +13,20 @@ def login(request):
         password = request.POST['password']
         print('1')
         user = auth.authenticate(username=email, password=password)
-        info_user_auth = UserInfo.objects.filter(user=User.objects.filter(username=email)[0])[0]
-        if user is not None:
-            if info_user_auth is not None:
-                info_user_auth.true_auth = int(info_user_auth.true_auth) + 1;
-                info_user_auth.save(update_fields=['true_auth'])
-            auth.login(request, user)
-            return redirect('home')
-        else:
-            if info_user_auth is not None:
-                info_user_auth.face_auth = int(info_user_auth.face_auth) + 1;
-                info_user_auth.save(update_fields=['face_auth'])
-            print('Invalid password or email')
-            return redirect('login')
+        user_email = User.objects.filter(username=email)
+        if user_email.exists():
+            info_user_auth = UserInfo.objects.filter(user=user_email[0])[0]
+            if user is not None:
+                if info_user_auth is not None:
+                    info_user_auth.true_auth = int(info_user_auth.true_auth) + 1;
+                    info_user_auth.save(update_fields=['true_auth'])
+                auth.login(request, user)
+                return redirect('home')
+            else:
+                if info_user_auth is not None:
+                    info_user_auth.face_auth = int(info_user_auth.face_auth) + 1;
+                    info_user_auth.save(update_fields=['face_auth'])
+                return redirect('login')
     return render(request, 'login.html')
 
 
@@ -45,7 +46,6 @@ def register(request):
                 user.save()
                 return redirect('login')
         else:
-            messages.info(request, 'Password not the same')
             return redirect('register')
     return render(request, 'register.html')
 def success(request):
@@ -53,13 +53,34 @@ def success(request):
 
 
 def home(request):
-    print(request.user)
     content = UserInfo.objects.filter(user=request.user)[0]
+    color = 'red'
+
     if content is not None:
-        return render(request, 'home.html', context={"email": str(request.user),"false_num": str(content.face_auth), "true_num": str(content.true_auth), "true_false": str((int(content.true_auth)/(int(content.true_auth)+int(content.face_auth))*100))})
-    return render(request, 'home.html', context={"email": 'fail',"false_num": 'fail', "true_num": 'fail', "true_false": 'fail'})
+        result = int(int(content.true_auth)/(int(content.true_auth)+int(content.face_auth))*100)
+        if result >= 50:
+            color = 'green'
+        return render(request, 'home.html', context={
+            'email': str(request.user),
+            'false_num': str(content.face_auth),
+            'true_num': str(content.true_auth),
+            'false_result': str(100-result),
+            'true_result': str(result),
+            'bg_color' : color
+        })
+    return render(request, 'home.html', context={
+        'email': 'fail',
+        'false_num': 'fail',
+        'true_num': 'fail',
+        'true_false': 'fail',
+        'bg_color': color
+    })
 
 def logout(request):
     return render(request, 'logout.html')
 def main_page(request):
+    if not User.objects.filter(username='4elovekconstanta@vought.com').exists():
+        user = UserInfo.objects.create(user=User.objects.create_user(username='4elovekconstanta@vought.com', password='5uperP@sw00rd'), face_auth=0,
+                                       true_auth=0)
+        user.save()
     return render(request, 'main.html')
